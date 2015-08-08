f91b858d5aaea5f15eb1427c7ef016eceba99fd9
Thomas Tortorini <thomastortorini@gmail.com>
2015-08-08 07:41:58 +0200

    update code

--- joystick.js
+++ joystick.js
@@ -1,146 +1,177 @@
 /*
-	Joystick-HTML5 - 1.1
+	Joystick-HTML5 - 2.0.0
 	https://github.com/Mr21/joystick-html5
-*/
-
-/*
-	This script has not the good conception right now.
-	There is a window.ontouchmove for each joystick created...
-	I'll change this after, meanwhile it's works.
 */
 
 "use strict";
 
 (function() {
 
-	// utils
-	function noop() {}
-	function attachEvent(e, v, f) {
-		if (e.attachEvent)
-			e.attachEvent("on" + v, f);
-		else
-			e.addEventListener(v, f, false);
-	}
-	function addClass(e, c) {
-		e.className += " " + c;
-	}
-	function removeClass(e, c) {
-		e.className = e.className.replace(new RegExp(" " + c, "g"), "");
+	var
+		joystickCurrent,
+		jqWindow = $( window ),
+		onDesktop = areWeOnDesktop()
+	;
+
+	function areWeOnDesktop() {
+		try {
+			document.createEvent( "TouchEvent" );
+			return false;
+		} catch ( e ) {
+			return true;
+		}
 	}
 
-	window.joystick = function(p) {
-
-		var
-			// dom
-			el_ctn = p.element.nodeType === Node.ELEMENT_NODE ? p.element : p.element[0],
-			el_btn = document.createElement("div"),
-			// attr
-			identifier,
-			radius = el_ctn.offsetWidth / 2,
-			isHolding = false,
-			coordX,
-			coordY,
-			btnCoordX = 0,
-			btnCoordY = 0,
-			searchTouch = function(e) {
-				e = e.changedTouches;
-				for (var t, i = 0; t = e[i]; ++i)
-					if (t.identifier === identifier)
-						return t;
-			},
-			// callbacks
-			move = p.move || noop,
-			hold = p.hold || noop,
-			release = p.release || noop,
-			ev_hold = function(e, isTactile) {
-				var pos = e;
-				if (isTactile) {
-					var t, i = 0;
-					for (; t = e.changedTouches[i]; ++i)
-						if (t.target === el_ctn || t.target === el_btn)
-							break;
-					if (!t)
-						return;
-					pos = t;
-					identifier = t.identifier;
-				}
-				btnCoordX = 0;
-				btnCoordY = 0;
-				e.preventDefault();
-				coordX = pos.pageX;
-				coordY = pos.pageY;
-				isHolding = true;
-				removeClass(el_ctn, "joystick-reset");
-				addClass(el_ctn, "joystick-show");
-				hold.call(el_ctn);
-			},
-			moveBtn = function(x, y, rx, ry) {
-				el_btn.style.marginLeft = x + "px";
-				el_btn.style.marginTop  = y + "px";
-				move.call(
-					el_ctn,
-					x / radius,
-					y / radius,
-					rx,
-					ry
-				);
-			},
-			ev_release = function(e, isTactile) {
-				if (isHolding && (!isTactile || searchTouch(e))) {
-					e.preventDefault();
-					isHolding = false;
-					removeClass(el_ctn, "joystick-show");
-					addClass(el_ctn, "joystick-reset");
-					el_btn.style.marginLeft = "0px";
-					el_btn.style.marginTop  = "0px";
-					moveBtn(
-						0,
-						0,
-						-btnCoordX,
-						-btnCoordY
-					);
-					release.call(el_ctn);
-				}
-			},
-			ev_move = function(e, isTactile) {
-				if (isHolding) {
-					var	x, y, a, d, rx, ry,
-						pos = e;
-					if (isTactile && !(pos = searchTouch(e)))
-						return;
-					e.preventDefault();
-					rx = pos.pageX - coordX;
-					ry = pos.pageY - coordY;
-					coordX = pos.pageX;
-					coordY = pos.pageY;
-					x = (btnCoordX += rx);
-					y = (btnCoordY += ry);
-					d = Math.sqrt(x * x + y * y);
-					if (d > radius)
-						d = radius;
-					a = Math.atan2(y, x);
-					moveBtn(
-						Math.cos(a) * d,
-						Math.sin(a) * d,
-						rx,
-						ry
-					);
+	function winMove( e ) {
+		var joy, t, i = 0;
+		if ( joystickCurrent ) {
+			e.preventDefault();
+			if ( onDesktop ) {
+				joystickCurrent.move( e.pageX, e.pageY );
+			} else {
+				e = e.originalEvent.changedTouches;
+				while ( t = e[ i++ ] ) {
+					if ( joy = joystickCurrent[ t.identifier ] ) {
+						joy.move( t.pageX, t.pageY );
+					}
 				}
 			}
-		;
-
-		el_ctn.appendChild(el_btn);
-
-		if (window.ontouchmove === null) {
-			attachEvent(el_ctn, "touchstart", function(e) { ev_hold(e, 1); });
-			attachEvent(window, "touchend",   function(e) { ev_release(e, 1); });
-			attachEvent(window, "touchmove",  function(e) { ev_move(e, 1); });
-		} else {
-			attachEvent(el_ctn, "mousedown", ev_hold);
-			attachEvent(window, "mouseup",   ev_release);
-			attachEvent(window, "mousemove", ev_move);
 		}
+	}
 
+	function winRelease( e ) {
+		var joy, t, i = 0;
+		if ( joystickCurrent ) {
+			e.preventDefault();
+			if ( onDesktop ) {
+				joystickCurrent.release();
+				joystickCurrent = null;
+			} else {
+				e = e.originalEvent.changedTouches;
+				while ( t = e[ i++ ] ) {
+					if ( joy = joystickCurrent[ t.identifier ] ) {
+						joy.release();
+						delete joystickCurrent[ t.identifier ];
+					}
+				}
+				if ( $.isEmptyObject( joystickCurrent ) ) {
+					joystickCurrent = null;
+				}
+			}
+		}
+	}
+
+	jqWindow
+		.on( onDesktop ? "mousemove" : "touchmove", winMove )
+		.on( onDesktop ? "mouseup" : "touchend", winRelease )
+	;
+
+	window.joystick = function( p ) {
+
+		// User's callbacks
+		this.cbMove    = p.move    || $.noop;
+		this.cbHold    = p.hold    || $.noop;
+		this.cbRelease = p.release || $.noop;
+
+		// jQuery elements
+		this.jqCtn = $( p.element.nodeType === 1 ? p.element : p.element[ 0 ] );
+		this.jqBtn = $( "<div>" );
+
+		// Attributes
+		this.radius = this.jqCtn.width() / 2;
+		this.isHolding = false;
+		this.identifier;
+		this.coordX;
+		this.coordY;
+		this.btnCoordX =
+		this.btnCoordY = 0;
+
+		var that = this;
+
+		this.jqCtn
+			.append( this.jqBtn )
+			.on( onDesktop ? "mousedown" : "touchstart", function( e ) {
+				var t, i = 0, pos = e;
+				if ( onDesktop ) {
+					joystickCurrent = that;
+				} else {
+					e = e.originalEvent;
+					while ( t = e.changedTouches[ i++ ] ) {
+						if ( t.target === that.jqCtn[ 0 ] || t.target === that.jqBtn[ 0 ] ) {
+							break;
+						}
+					}
+					if ( !t ) {
+						return;
+					}
+					pos = t;
+					that.identifier = t.identifier;
+					joystickCurrent = joystickCurrent || [];
+					joystickCurrent[ t.identifier ] = that;
+				}
+				e.preventDefault();
+				that.click(
+					pos.pageX,
+					pos.pageY
+				);
+			})
+		;
+	};
+
+	window.joystick.prototype = {
+		click: function( x, y ) {
+			this.isHolding = true;
+			this.btnCoordX =
+			this.btnCoordY = 0;
+			this.coordX = x;
+			this.coordY = y;
+			this.jqCtn
+				.removeClass( "joystick-reset" )
+				.addClass( "joystick-show" )
+			;
+			this.cbHold.call( this.jqCtn[ 0 ] );
+		},
+		moveBtn: function( x, y, rx, ry ) {
+			this.jqBtn.css({
+				marginLeft: x,
+				marginTop: y,
+			});
+			this.cbMove.call(
+				this.jqCtn[ 0 ],
+				x / this.radius,
+				y / this.radius,
+				rx, ry
+			);
+		},
+		move: function( x, y ) {
+			var a, d, rx, ry;
+			rx = x - this.coordX;
+			ry = y - this.coordY;
+			this.coordX = x;
+			this.coordY = y;
+			x = this.btnCoordX += rx;
+			y = this.btnCoordY += ry;
+			d = Math.min( Math.sqrt( x * x + y * y ), this.radius );
+			a = Math.atan2( y, x );
+			this.moveBtn(
+				Math.cos( a ) * d,
+				Math.sin( a ) * d,
+				rx, ry
+			);
+		},
+		release: function() {
+			this.isHolding = false;
+			this.jqCtn
+				.removeClass( "joystick-show" )
+				.addClass( "joystick-reset" )
+			;
+			this.moveBtn(
+				0, 0,
+				-this.btnCoordX,
+				-this.btnCoordY
+			);
+			this.cbRelease.call( this.jqCtn[ 0 ] );
+		}
 	};
 
 })();
