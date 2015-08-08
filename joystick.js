728691e2c9213b91f873073993387b24bc44c893
Thomas Tortorini <thomastortorini@gmail.com>
2015-08-08 06:11:09 +0200

    difference between the Math code and the Event code

--- joystick.js
+++ joystick.js
@@ -15,9 +15,9 @@
 	window.joystick = function( p ) {
 
 		// User's callbacks
-		this.move    = p.move    || $.noop;
-		this.hold    = p.hold    || $.noop;
-		this.release = p.release || $.noop;
+		this.cbMove    = p.move    || $.noop;
+		this.cbHold    = p.hold    || $.noop;
+		this.cbRelease = p.release || $.noop;
 
 		// jQuery elements
 		this.jqCtn = $( p.element.nodeType === 1 ? p.element : p.element[ 0 ] );
@@ -47,6 +47,62 @@
 	};
 
 	window.joystick.prototype = {
+		click: function( x, y ) {
+			this.btnCoordX =
+			this.btnCoordY = 0;
+			this.coordX = x;
+			this.coordY = y;
+			this.isHolding = true;
+			this.jqCtn
+				.removeClass( "joystick-reset" )
+				.addClass( "joystick-show" )
+			;
+			this.cbHold.call( this.jqCtn[ 0 ] );
+		},
+		moveBtn: function( x, y, rx, ry ) {
+			this.jqBtn.css({
+				marginLeft: x,
+				marginTop: y,
+			});
+			this.cbMove.call(
+				this.jqCtn[ 0 ],
+				x / this.radius,
+				y / this.radius,
+				rx, ry
+			);
+		},
+		move: function( x, y ) {
+			var a, d, rx, ry;
+			rx = x - this.coordX;
+			ry = y - this.coordY;
+			this.coordX = x;
+			this.coordY = y;
+			x = this.btnCoordX += rx;
+			y = this.btnCoordY += ry;
+			d = Math.min( Math.sqrt( x * x + y * y ), this.radius );
+			a = Math.atan2( y, x );
+			this.moveBtn(
+				Math.cos( a ) * d,
+				Math.sin( a ) * d,
+				rx, ry
+			);
+		},
+		release: function() {
+			this.isHolding = false;
+			this.jqCtn
+				.removeClass( "joystick-show" )
+				.addClass( "joystick-reset" )
+			;
+			this.moveBtn(
+				0, 0,
+				-this.btnCoordX,
+				-this.btnCoordY
+			);
+			this.cbRelease.call( this.jqCtn[ 0 ] );
+		},
+
+
+
 		searchTouch: function( e ) {
 			var t, i = 0;
 			e = e.changedTouches;
@@ -70,78 +126,28 @@
 				pos = t;
 				this.identifier = t.identifier;
 			}
-			this.btnCoordX = 0;
-			this.btnCoordY = 0;
 			e.preventDefault();
-			this.coordX = pos.pageX;
-			this.coordY = pos.pageY;
-			this.isHolding = true;
-			this.jqCtn
-				.removeClass( "joystick-reset" )
-				.addClass( "joystick-show" )
-			;
-			this.hold.call( this.jqCtn[ 0 ] );
-		},
-		moveBtn: function( x, y, rx, ry ) {
-			this.jqBtn.css({
-				marginLeft: x,
-				marginTop: y,
-			});
-			this.move.call(
-				this.jqCtn[ 0 ],
-				x / this.radius,
-				y / this.radius,
-				rx,
-				ry
+			this.click(
+				pos.pageX,
+				pos.pageY
 			);
 		},
 		ev_release: function( e ) {
 			if ( this.isHolding && ( !isTactile || searchTouch( e ) ) ) {
 				e.preventDefault();
-				this.isHolding = false;
-				this.jqCtn
-					.removeClass( "joystick-show" )
-					.addClass( "joystick-reset" )
-				;
-				this.jqBtn.css({
-					marginLeft: 0,
-					marginTop: 0
-				});
-				this.moveBtn(
-					0,
-					0,
-					-this.btnCoordX,
-					-this.btnCoordY
-				);
-				this.release.call( this.jqCtn[ 0 ] );
+				this.release();
 			}
 		},
 		ev_move: function( e ) {
 			if ( this.isHolding ) {
-				var
-					x, y, a, d, rx, ry,
-					pos = e
-				;
+				var pos = e;
 				if ( isTactile && !( pos = searchTouch( e ) ) ) {
 					return;
 				}
 				e.preventDefault();
-				rx = pos.pageX - this.coordX;
-				ry = pos.pageY - this.coordY;
-				this.coordX = pos.pageX;
-				this.coordY = pos.pageY;
-				x = this.btnCoordX += rx;
-				y = this.btnCoordY += ry;
-				d = Math.sqrt( x * x + y * y );
-				if ( d > this.radius ) {
-					d = this.radius;
-				}
-				a = Math.atan2( y, x );
-				this.moveBtn(
-					Math.cos( a ) * d,
-					Math.sin( a ) * d,
-					rx,
-					ry
+				this.move(
+					pos.pageX,
+					pos.pageY
 				);
 			}
 		}
