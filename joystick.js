8009ce47a0ea9b29b4bede6894f5f3475c353d98
Thomas Tortorini <thomastortorini@gmail.com>
2015-08-09 07:08:22 +0200

    this joystick are now a "jquery-element", no more CSS file, instant initialisation, etc.

--- joystick.js
+++ joystick.js
@@ -3,82 +3,139 @@
 	https://github.com/Mr21/joystick-html5
 */
 
+(function( $ ) {
+
 "use strict";
 
-(function() {
+var
+	joystickCurrent,
+	jqWindow = $( window ),
+	onDesktop = areWeOnDesktop()
+;
 
-	var
-		joystickCurrent,
-		jqWindow = $( window ),
-		onDesktop = areWeOnDesktop()
-	;
-
-	function areWeOnDesktop() {
-		try {
-			document.createEvent( "TouchEvent" );
-			return false;
-		} catch ( e ) {
-			return true;
-		}
+function areWeOnDesktop() {
+	try {
+		document.createEvent( "TouchEvent" );
+		return false;
+	} catch ( e ) {
+		return true;
 	}
+}
 
-	function winMove( e ) {
-		var joy, t, i = 0;
-		if ( joystickCurrent ) {
-			e.preventDefault();
-			if ( onDesktop ) {
-				joystickCurrent.move( e.pageX, e.pageY );
-			} else {
-				e = e.originalEvent.changedTouches;
-				while ( t = e[ i++ ] ) {
-					if ( joy = joystickCurrent[ t.identifier ] ) {
-						joy.move( t.pageX, t.pageY );
-					}
+function winMove( e ) {
+	var joy, t, i = 0;
+	if ( joystickCurrent ) {
+		e.preventDefault();
+		if ( onDesktop ) {
+			joystickCurrent.move( e.pageX, e.pageY );
+		} else {
+			e = e.originalEvent.changedTouches;
+			while ( t = e[ i++ ] ) {
+				if ( joy = joystickCurrent[ t.identifier ] ) {
+					joy.move( t.pageX, t.pageY );
 				}
 			}
 		}
 	}
+}
 
-	function winRelease( e ) {
-		var joy, t, i = 0;
-		if ( joystickCurrent ) {
-			e.preventDefault();
-			if ( onDesktop ) {
-				joystickCurrent.release();
+function winRelease( e ) {
+	var joy, t, i = 0;
+	if ( joystickCurrent ) {
+		e.preventDefault();
+		if ( onDesktop ) {
+			joystickCurrent.release();
+			joystickCurrent = null;
+		} else {
+			e = e.originalEvent.changedTouches;
+			while ( t = e[ i++ ] ) {
+				if ( joy = joystickCurrent[ t.identifier ] ) {
+					joy.release();
+					delete joystickCurrent[ t.identifier ];
+				}
+			}
+			if ( $.isEmptyObject( joystickCurrent ) ) {
 				joystickCurrent = null;
-			} else {
-				e = e.originalEvent.changedTouches;
-				while ( t = e[ i++ ] ) {
-					if ( joy = joystickCurrent[ t.identifier ] ) {
-						joy.release();
-						delete joystickCurrent[ t.identifier ];
-					}
-				}
-				if ( $.isEmptyObject( joystickCurrent ) ) {
-					joystickCurrent = null;
-				}
 			}
 		}
 	}
+}
 
-	jqWindow
-		.on( onDesktop ? "mousemove" : "touchmove", winMove )
-		.on( onDesktop ? "mouseup" : "touchend", winRelease )
-	;
+jqWindow
+	.on( onDesktop ? "mousemove" : "touchmove", winMove )
+	.on( onDesktop ? "mouseup" : "touchend", winRelease )
+;
 
-	window.joystick = function( p ) {
+$.element({
+	name: "joystick",
+	html: "<div></div>",
+	css: "\
+		.joystick {\
+			display: inline-block;\
+			position: relative;\
+			width: 100px;\
+			height: 100px;\
+			border: 1px solid;\
+			border-radius: 50%;\
+			transition: opacity 1s ease;\
+			opacity: .3;\
+			cursor: move;\
+		}\
+		.joystick,\
+		.joystick * {\
+			box-sizing: border-box;\
+		}\
+		.joystick.joystick-show {\
+			transition-duration: .2s;\
+			opacity: 1;\
+		}\
+		.joystick:before,\
+		.joystick * {\
+			position: absolute;\
+			width: 50%;\
+			height: 50%;\
+			left: 25%;\
+			top: 25%;\
+			border-radius: 50%;\
+		}\
+		.joystick:before {\
+			content: '';\
+			box-sizing: content-box;\
+			margin: -7px;\
+			border: 2px dashed;\
+			padding: 5px;\
+			opacity: .5;\
+		}\
+		.joystick * {\
+			border: 2px solid;\
+			background-position: center;\
+			transition-property: border-width;\
+			transition-duration: inherit;\
+			transition-timing-function: inherit;\
+		}\
+		.joystick.joystick-show * {\
+			border-width: 5px;\
+		}\
+		.joystick.joystick-reset * {\
+			transition-property: border-width, margin;\
+			transition-duration: .1s;\
+		}\
+	",
+	init: function() {
 
-		// User's callbacks
-		this.cbMove    = p.move    || $.noop;
-		this.cbHold    = p.hold    || $.noop;
-		this.cbRelease = p.release || $.noop;
+		// First, initialize all the user's callback.
+		this.init({});
 
-		// jQuery elements
-		this.jqCtn = $( p.element.nodeType === 1 ? p.element : p.element[ 0 ] );
-		this.jqBtn = $( "<div>" );
+		// This is the bouton inside the joystick himself.
+		// this.jqElement IS the joystick himself (the container).
+		this.jqBtn =
+			this.jqElement
+				.addClass( "joystick" )
+				.children()
+			;
 
-		// Attributes
-		this.radius = this.jqCtn.width() / 2;
+		// Attributes.
+		this.radius = this.jqElement.width() / 2;
 		this.isHolding = false;
 		this.identifier;
 		this.coordX;
@@ -88,8 +145,7 @@
 
 		var that = this;
 
-		this.jqCtn
-			.append( this.jqBtn )
+		this.jqElement
 			.on( onDesktop ? "mousedown" : "touchstart", function( e ) {
 				var t, i = 0, pos = e;
 				if ( onDesktop ) {
@@ -97,7 +153,7 @@
 				} else {
 					e = e.originalEvent;
 					while ( t = e.changedTouches[ i++ ] ) {
-						if ( t.target === that.jqCtn[ 0 ] || t.target === that.jqBtn[ 0 ] ) {
+						if ( t.target === that.jqElement[ 0 ] || t.target === that.jqBtn[ 0 ] ) {
 							break;
 						}
 					}
@@ -116,20 +172,29 @@
 				);
 			})
 		;
-	};
+	},
+	prototype: {
 
-	window.joystick.prototype = {
+		// public:
+		init: function( p ) {
+			// User's callbacks.
+			this.cbMove    = p.move    || $.noop;
+			this.cbHold    = p.hold    || $.noop;
+			this.cbRelease = p.release || $.noop;
+		},
+
+		// private:
 		click: function( x, y ) {
 			this.isHolding = true;
 			this.btnCoordX =
 			this.btnCoordY = 0;
 			this.coordX = x;
 			this.coordY = y;
-			this.jqCtn
+			this.jqElement
 				.removeClass( "joystick-reset" )
 				.addClass( "joystick-show" )
 			;
-			this.cbHold.call( this.jqCtn[ 0 ] );
+			this.cbHold.call( this.jqElement[ 0 ] );
 		},
 		moveBtn: function( x, y, rx, ry ) {
 			this.jqBtn.css({
@@ -137,7 +202,7 @@
 				marginTop: y,
 			});
 			this.cbMove.call(
-				this.jqCtn[ 0 ],
+				this.jqElement[ 0 ],
 				x / this.radius,
 				y / this.radius,
 				rx, ry
@@ -161,7 +226,7 @@
 		},
 		release: function() {
 			this.isHolding = false;
-			this.jqCtn
+			this.jqElement
 				.removeClass( "joystick-show" )
 				.addClass( "joystick-reset" )
 			;
@@ -170,8 +235,9 @@
 				-this.btnCoordX,
 				-this.btnCoordY
 			);
-			this.cbRelease.call( this.jqCtn[ 0 ] );
+			this.cbRelease.call( this.jqElement[ 0 ] );
 		}
-	};
+	}
+});
 
-})();
+})( jQuery );
